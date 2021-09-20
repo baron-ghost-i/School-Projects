b723f870b3586497f8325c94e28911d8e182bafe
baron-ghost-i <85233404+baron-ghost-i@users.noreply.github.com>
2021-09-20 19:42:41 +0530

    Removed unnecessary lines

--- 2_swapper.py
+++ 2_swapper.py
@@ -1,8 +1,6 @@
 x = input("Enter the list (separate each value with a whitespace): ")
 
 x = x.split(" ")
-for i in range(len(x)):
-    x[i] = int(x[i])
 print(f"Before swapping: {x}")
 
 element = x.pop(len(x)-1)
